bf37df688a82d2ce2cc9f933315e0f1467afe31b
william <Williaminteligenciaartificial@users.noreply.github.com>
2025-04-02 04:33:23 -0500

    porta-02-abril

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,9 @@ import activeMenu from './components/selected_menu.js';
 import updateCopyright from './helpers/date_updater.js';
 import resetToHome from './helpers/reload_page.js';
 import sendEmail from './helpers/send_form.js';
+import { changeLanguage,detectLanguage } from '../lang/lenguaje.js';
+
+
 
 parallax();
 
@@ -13,3 +16,11 @@ resetToHome();
 updateCopyright();
 
 sendEmail();
+
+detectLanguage();
+
+document.getElementById("btn-es").addEventListener("click", () => changeLanguage("es"));
+document.getElementById("btn-en").addEventListener("click", () => changeLanguage("en"));
+
+
+
